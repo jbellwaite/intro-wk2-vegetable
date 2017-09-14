4618df1ce037139e22859f265025677fed359187
Jessie Waite and Michael Arrington <michael@>
2017-09-14 11:00:32 -0700

    Add images and description to answer output

--- js/scripts.js
+++ js/scripts.js
@@ -5,14 +5,29 @@ $(document).ready(function() {
     // var userScore = age + color + season ;
     var userScore = parseInt($("input:radio[name=age]:checked").val()) + parseInt($("input:radio[name=color]:checked").val()) + parseInt($("input:radio[name=season]:checked").val()) ;
 
-    if (userScore === 3) {
-      $("#answer").text("You are a vegetable");
+    if (userScore === 3 || userScore === 4) {
+      $("#answer").text("You are a beet!");
+      $("#beet").removeClass();
     }
-    else if (userScore === 4 || userScore === 5) {
-      $("#answer").text("You are a vegetable2");
+    else if (userScore === 5 || userScore === 6) {
+      $("#answer").text("You are a bellpepper!");
+      $("#bellpepper").removeClass();
+    }
+    else if (userScore === 7) {
+      $("#answer").text("You are a broccoli!");
+      $("#broccoli").removeClass();
+    }
+    else if (userScore === 8) {
+      $("#answer").text("You are brussel sprouts!");
+      $("#brussels").removeClass();
+    }
+    else if (userScore === 9 || userScore === 10) {
+      $("#answer").text("You are a tomato (its actually vegetable)!");
+      $("#tomato").removeClass();
     }
     else {
-      $("#answer").text("You are a vegetable3");
+      $("#answer").text("You are a carrot!");
+      $("#carrot").removeClass();
     }
 
     $("#vegetable").hide();
